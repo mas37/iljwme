1dbd414daf60743621003792f5732232ef259b9c
Alexander Shapeev <shapeev@gmail.com>
2020-07-11 20:58:38 +0300

    example 2 updated

--- doc/examples/install.sh
+++ doc/examples/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O example-2.tar.xz https://www.dropbox.com/s/avxenoktcm6j6es/example-2.tar.xz?dl=0
+wget -O example-2.tar.xz https://www.dropbox.com/s/i43cup4d6ftjieb/example-2.tar.xz?dl=0
 wget -O example-3.tar.xz https://www.dropbox.com/s/lo72s1m35rmlvw0/example-3.tar.xz?dl=0
 
 tar -xJvf example-2.tar.xz
