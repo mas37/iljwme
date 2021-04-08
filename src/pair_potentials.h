4cea9261825013699c1a39f13290235fb4acb635
Ivan Novikov <i.novikov@skoltech.ru>
2021-04-08 14:11:21 +0300

    started repulsion function coding

--- src/pair_potentials.h
+++ src/pair_potentials.h
@@ -133,5 +133,86 @@ public:
 	}
 };
 
+class ZBL : public PairPotential, protected InitBySettings
+{
+private:
+    const std::string filename;
+    int atom_types_in_substance_count;
+    double r_min;
+    std::vector<int> z;
+    const double m1 = 0.18175;
+    const double m2 = 0.50986;
+    const double m3 = 0.28022;
+    const double m4 = 0.02817;
+    const double p1 = -3.1998;
+    const double p2 = -0.94229;
+    const double p3 = -0.4029;
+    const double p4 = -0.20162;
+    
+/*    void InitSettings() // Sets correspondence between variables and setting names in settings file
+    {
+        MakeSetting(r_min, "r_min");
+        MakeSetting(atom_types_in_substance_count, "n_atomic_types");
+        MakeSetting(cutoff, "cutoff");
+    }
+*/
+
+public:
+    void Load(const std::string& filename) {
+        ;
+    } 
+
+    ZBL(const std::string& filename_) :
+        filename(filename_)
+    {
+        Load(filename);
+    }
+    
+    double F(double r, double a)
+    {
+        double phi = 0;
+        phi += m1 * exp(p1 * r / a);
+        phi += m2 * exp(p2 * r / a);
+        phi += m3 * exp(p3 * r / a);
+        phi += m4 * exp(p4 * r / a);
+        return phi / r;
+    }
+    double dF_dr(double r, double a)
+    {
+        double phi = 0;
+        phi += m1 * exp(p1 * r / a);
+        phi += m2 * exp(p2 * r / a);
+        phi += m3 * exp(p3 * r / a);
+        phi += m4 * exp(p4 * r / a);
+        double d_phi_dr = 0;
+        d_phi_dr += p1 * m1 * exp(p1 * r / a) / a;
+        d_phi_dr += p2 * m2 * exp(p2 * r / a) / a;
+        d_phi_dr += p3 * m3 * exp(p3 * r / a) / a;
+        d_phi_dr += p4 * m4 * exp(p4 * r / a) / a;
+        return - phi / (r * r) + d_phi_dr / r;
+    }
+    
+    double f_repulsion(double r)
+    {
+        double A = -1 * (cutoff - r_min) / (cutoff - r);
+        double B = exp((cutoff - r) / (r_min - r));
+        if (r < 1.0001 * r_min) return 1;
+        else if (1.0001 * r_min <= r < 0.9999 * cutoff) return exp(A * B);
+        else return 0;
+    }
+    double df_repulsion_dr(double r)
+    {
+        double A = -1 * (cutoff - r_min) / (cutoff - r);
+        double B = exp((cutoff - r) / (r_min - r));
+        double dA_dr = -1 * (cutoff - r_min)  / ((cutoff - r) * (cutoff - r));
+        double dB_dr = B * (- 1.0 / (r_min - r) + (cutoff - r) / (r_min - r)*(r_min - r));
+        if (1.0001 * r_min <= r < 0.9999 * cutoff) return exp(A * B) * (dA_dr * B + A * dB_dr);
+        else return 0;
+    }
+
+    ~ZBL(){};
+
+};
+
 #endif //#ifndef MLIP_PAIR_POTENTIALS_H
 
